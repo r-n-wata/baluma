42e7ddb09ab5c88fb7d9f1ea1a699224ccd14da0
r-n-wata <ruthnwata@gmail.com>
2024-08-03 22:32:34 -0500

    add: build folder and adjust cpanel

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useCallback, ReactNode } from "react";
+import { createContext, useState, useCallback, ReactNode } from "react";
 
 interface LanguageProviderProps {
   children: ReactNode;
