19344a7419b12bd58668ee6e1db6f851d9ed30e4
Ruth Wata <88207822+r-n-wata@users.noreply.github.com>
2026-06-24 13:20:14 -0500

    Refresh homepage gallery assets and hero image

--- src/data/homeImgs.ts
+++ src/data/homeImgs.ts
@@ -1,10 +1,49 @@
 import h1 from "../assets/home/h1.jpg";
 import h2 from "../assets/home/h2.jpg";
-import h4 from "../assets/home/h4.jpg";
 import h5 from "../assets/home/h5.jpg";
 import h6 from "../assets/home/h6.jpg";
 import h7 from "../assets/home/h7.jpeg";
 import h8 from "../assets/home/h8.jpeg";
+import h9 from "../assets/home/h9.jpg";
+import h10 from "../assets/home/h10.jpg";
+import h11 from "../assets/home/h11.jpg";
+import h12 from "../assets/home/h12.jpg";
+import h13 from "../assets/home/h13.jpg";
+import h14 from "../assets/home/h14.jpg";
+import h15 from "../assets/home/h15.jpg";
+import h16 from "../assets/home/h16.jpg";
+import h17 from "../assets/home/h17.webp";
+import h18 from "../assets/home/h18.webp";
+import h19 from "../assets/home/h19.webp";
+import h21 from "../assets/home/h21.jpg";
+import h22 from "../assets/home/h22.webp";
+import h23 from "../assets/home/h23.webp";
+import h24 from "../assets/home/h24.webp";
+import h25 from "../assets/home/h25.webp";
 import originalHero from "../assets/background2.jpg";
 
-export const homeImgs = [h1, h2, h4, h5, h6, h7, h8, originalHero];
+export const homeImgs = [
+  h1,
+  h2,
+  h5,
+  h6,
+  h7,
+  h8,
+  h9,
+  h10,
+  h11,
+  h12,
+  h13,
+  h14,
+  h15,
+  h16,
+  h17,
+  h18,
+  h19,
+  h21,
+  h22,
+  h23,
+  h24,
+  h25,
+  originalHero,
+];
